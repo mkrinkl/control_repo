cfa53e3d1571e6a8da6ccf53c357d5ede8e2dd6d
Bruno <bruno.s@tentwentyfour.lu>
2021-06-06 15:45:55 +0200

    changing node fqdn

--- site-modules/profile/manifests/hosts.pp
+++ site-modules/profile/manifests/hosts.pp
@@ -1,19 +1,19 @@
 class profile::hosts {
-  host { '6f25ccf20b1c.mylabserver.com':
+  host { '2781cef8391c.mylabserver.com':
     ip           => '18.132.41.247',
     host_aliases => [ puppet ],
   }
-  host { 'e0049fc5161c.mylabserver.com':
+  host { '75f069785c1c.mylabserver.com':
     ip           => '3.8.77.154',
     host_aliases => [ loadbalancer, lb-1, lb ],
   }
 
-  host { 'b7bd49091a1c.mylabserver.com':
+  host { 'd12f8b10791c.mylabserver.com':
     ip           => '3.9.19.249',
     host_aliases => [ webserver1, web1 ],
   }
 
-  host { '813f4b351e1c.mylabserver.com':
+  host { 'dd19bcbb621c.mylabserver.com':
     ip           => '3.8.78.219',
     host_aliases => [ webserver2, web2 ],
   }
